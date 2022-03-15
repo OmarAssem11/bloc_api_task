2306fa55705eedd9a50565d9bfdb70e384362a1b
Omar Assem <omarassem996@gmail.com>
2022-03-15 19:40:53 +0200

    retrofit-getit-freezed

--- lib/widgets/launch_widget.dart
+++ lib/widgets/launch_widget.dart
@@ -1,9 +1,6 @@
-import 'package:bloc_api_task/blocs/cubit/launches_cubit.dart';
-import 'package:bloc_api_task/blocs/cubit/launches_union.dart';
 import 'package:bloc_api_task/get_it/get_it.dart';
 import 'package:bloc_api_task/screens/launch_details_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LaunchWidget extends StatelessWidget {
   const LaunchWidget(this.index);
